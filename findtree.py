0beee6fb7677c315b2c38fe334b84df272a285dd
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2018-09-19 10:24:51 +0200

    Finished implementation of findtree

--- findtree.py
+++ findtree.py
@@ -1,35 +1,56 @@
-#
-# Piped with output from the find command, e.g.:
-# find . -name '*.ipynb' ! -name '*-checkpoint.ipynb'
-#
+"""
+Visualize the file system structure 
+of the result of find command. 
 
-# TODO
+Usage example:
+find . -name "*.ipynb" ! -name "*-checkpoint.ipynb" | python /path/to/findtree.py
+"""
 
 import fileinput
 import re
+from pprint import pprint
 
-res = {}
 
-for line in fileinput.input():
+def insert(d, key):
+    if key not in d:
+        d[key] = {}
+
+
+def fill(d, first, rest):
     
-    line = line[2:]
-    #print(line)
-    #continue
-    
-    elements = line.split('/')
-    
-    d = res
-    for el in elements[:-2]:
-        if el not in d:
-            d[el] = {}
-        d = d[el]
+    insert(d, first)
+
+    parent = d[first]
+    for el in rest:
+        insert(parent, el)
+        parent = parent[el]
+
+
+def print_tree(d):
+
+    def p(prefix, d):
+
+        for k, v in d.items():
+            print('{}{}'.format(prefix, k))
+            p(prefix+'\t', v)
+
+    p('', d)
+
+
+if __name__ == '__main__':
+
+    d = {}
+
+    for line in fileinput.input():
         
-    last_dir = elements[-2]
-    fname = elements[-1]
-    if last_dir not in d:
-        d[last_dir] = []   
-    d[last_dir] = fname  
-    
-    
-    
+        line = line[2:]
         
+        elements = line.split('/')
+        elements = list(map(lambda x: x.strip(), elements))
+
+        first = elements[0]
+        rest = elements[1:]
+
+        fill(d, first, rest)
+    
+    print_tree(d)            
