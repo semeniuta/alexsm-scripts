aee6a2869ed18c4bd31a07c93995b01c08e4da81
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2018-10-19 11:22:32 +0200

    Added the pdf2im shell script

---
+++ pdf2im.sh
@@ -0,0 +1,42 @@
+#/bin/sh
+
+#
+# The scripts uses ImageMagick to convert figures in PDF 
+# into the another format (e.g. JPEG, TIFF). 
+#
+# Converts all PDF files in the current directory 
+#
+# Can be supplied with the output directory 
+# or save the output files in the current directory.
+#
+# Example:
+# cd figures
+# mkdir ~/Desktop/out_tiff
+# /path/to/pdf2im.sh ~/Desktop/out_images jpg
+#
+
+if [ -z "$1" ]; then # checks for empty string
+    echo "Output directory is empty. Using current directory"
+    d="."
+else
+    d=$1
+fi
+
+echo "Output directory: $d"
+
+if [ -z "$2" ]; then 
+    echo "No output format specified. Using jpg"
+    extension="jpg"
+else
+    extension=$2
+fi
+
+echo "Output format: $extension"
+
+for f in *.pdf; do
+
+    fn="${f%.*}" # file name without extension
+
+    echo "Converting: $f -> $d/$fn.$extension"
+    convert -density 300x300 $f $d/$fn.$extension
+done;
