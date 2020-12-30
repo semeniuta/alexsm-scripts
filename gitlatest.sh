1230bdec8dacd072bb5a80de23822867a66f004a
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2020-12-30 19:42:48 +0100

    Improve gitlatest.sh

--- gitlatest.sh
+++ gitlatest.sh
@@ -2,4 +2,7 @@
 
 REPO_URL=$1
 
-git ls-remote --tags $REPO_URL | cut -f2 | sort | tail -n 1
+LAST=$(git ls-remote --tags $REPO_URL | cut -f 2 | sort | tail -n 1)
+
+# take out the first two fields (refs/tags/)
+echo $LAST | cut -d "/" -f 3-
