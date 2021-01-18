0aa7efb75f13b57ab775f47e7667e7d70a68cb28
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2021-01-18 14:22:03 +0100

    Implement get_set and compare in aptshow.py

--- aptshow.py
+++ aptshow.py
@@ -10,13 +10,33 @@ def run_apt_show(package_name):
     out = check_output(cmd, stderr=STDOUT)
     
     return out.split(b'\n')
+
+
+def get_set(out, prefix):
+    
+    for line in out:
+        if line.startswith(prefix):
+            packages = [el.strip().decode('utf-8') for el in line.split(b',')]
+            return set(packages)
+        
+    return set()
+
+    
+
+def compare(out_1, out_2):
+    
+    dep_1 = get_set(out_1, b'Depends')
+    dep_2 = get_set(out_2, b'Depends')
+    
+    print(dep_1 & dep_2)
      
      
 if __name__ == '__main__':
     
-    package_name = sys.argv[1]
+    package_1 = sys.argv[1]
+    package_2 = sys.argv[2]
     
-    res = run_apt_show(package_name)
+    out_1 = run_apt_show(package_1)
+    out_2 = run_apt_show(package_2)
     
-    for line in res:
-        print(line)
+    compare(out_1, out_2)
