5d17de066df071b9bd0656b6ce97880befca1adc
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2021-08-06 08:36:35 +0200

    Fix issues when cutting video in videoca.sh

--- videoca.sh
+++ videoca.sh
@@ -14,6 +14,8 @@ counter=0
 echo "" > "$filelist"
 commands=()
 
+#ffmpeg -i "$video_file" -c copy -bsf:v h264_mp4toannexb -f mpegts "$tmp_dir/video.ts"
+
 while read -r line
 do
     # Make array from the line (space-separated timrstamps)
@@ -22,7 +24,8 @@ do
     clip_start=${timestamps[0]}
     clip_end=${timestamps[1]}
 
-    cmd="ffmpeg -y -ss $clip_start -to $clip_end -i $video_file -c:v copy -c:a copy $tmp_dir/$counter.mp4"
+    cmd="ffmpeg -y -i $video_file -ss $clip_start -to $clip_end $tmp_dir/$counter.mp4"
+    
     commands+=("$cmd")
 
     echo "file '$counter.mp4'" >> "$filelist"
@@ -37,4 +40,4 @@ do
     eval "$cmd"
 done
 
-eval "ffmpeg -y -f concat -safe 0 -i $filelist -c copy $tmp_dir/merged.mp4"
+ffmpeg -y -f concat -safe 0 -i "$filelist" -c copy "$tmp_dir/merged.mp4"
