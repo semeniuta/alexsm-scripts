43438195c406635e8d5b128c1312839fad0986e9
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2021-08-06 09:32:00 +0200

    Add some comments to videoca.sh

--- videoca.sh
+++ videoca.sh
@@ -14,16 +14,15 @@ counter=0
 echo "" > "$filelist"
 commands=()
 
-#ffmpeg -i "$video_file" -c copy -bsf:v h264_mp4toannexb -f mpegts "$tmp_dir/video.ts"
-
 while read -r line
 do
-    # Make array from the line (space-separated timrstamps)
+    # Make array from the line (space-separated timestamps)
     timestamps=($line)
     
     clip_start=${timestamps[0]}
     clip_end=${timestamps[1]}
 
+    # Cut video section with slower seek & no copying of codecs
     cmd="ffmpeg -y -i $video_file -ss $clip_start -to $clip_end $tmp_dir/$counter.mp4"
     
     commands+=("$cmd")
@@ -34,10 +33,12 @@ do
 
 done < "$timestamps_file"
 
+# Execute each command for cutting video sections
 for cmd in "${commands[@]}"
 do
     echo "$cmd"
     eval "$cmd"
 done
 
+# Merge the clips into one file
 ffmpeg -y -f concat -safe 0 -i "$filelist" -c copy "$tmp_dir/merged.mp4"
